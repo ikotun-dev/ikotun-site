70528b51ae011dc18f6eca16d6391ca78cee67f1
Ikotun Collins <danlogan2003@gmail.com>
2023-05-02 17:47:35 +0100

    fixes

--- src/components/TopHead.vue
+++ src/components/TopHead.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-zinc-900 w-full  h-12 flex justify-center">
         <div class="mt-3 flex justify-between">
-            <i class="fab fa-twitter  text-white"></i>
-            <i class="fab fa-github ml-4 mr-4 text-white"></i>
-            <i class="fab fa-linkedin  text-white"></i>
+            <i class="fab fa-twitter hover:text-zinc-500 text-white"></i>
+            <i class="fab fa-github ml-4 mr-4 hover:text-zinc-500 text-white"></i>
+            <i class="fab fa-linkedin  hover:text-zinc-500 text-white"></i>
         
         </div>
 
