ef7dfc620b55beff3a5f16df8d9901cc5eadb9dc
Ikotun Collins <danlogan2003@gmail.com>
2023-05-03 20:29:48 +0100

    horizontal line

--- src/components/HeaDer.vue
+++ src/components/HeaDer.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="pt-10 pl-10 bg-zinc-900">
+    <div class="pt-10 pl-10  pb-5 bg-black">
           <img class="w-32 h-32 rounded-full" :src="require('../assets/images/832.jpg')" alt="my image">
+          <div class="flex mt-5">
+          <i class="fas fa-map-marker text-gray mr-4"></i><h3 class=" text-gray-600 text-xs">Lagos, Nigeria</h3>
+          </div>
+          <hr/>
+          <hr/>
     </div>
   </template>
-  
 
 <script>
 
