504330aadc08ea52ffc974b6ea2363157fefa99f
Ikotun Collins <danlogan2003@gmail.com>
2023-05-05 20:54:57 +0100

    changed height value

--- src/components/TopHead.vue
+++ src/components/TopHead.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-zinc-900 w-full  h-12 flex justify-center">
+    <div class="bg-zinc-900 w-full  h-32 flex justify-center">
         <div class="mt-3 flex justify-between">
             <i class="fab fa-twitter hover:text-zinc-500 text-white"></i>
             <i class="fab fa-github ml-4 mr-4 hover:text-zinc-500 text-white"></i>
