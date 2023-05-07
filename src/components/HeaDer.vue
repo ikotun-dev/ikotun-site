a594aa0afa30827ad516e70c374a943140252665
Ikotun Collins <danlogan2003@gmail.com>
2023-05-07 15:43:46 +0100

    fixes

--- src/components/HeaDer.vue
+++ src/components/HeaDer.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="pt-10 pl-10  pb-5 bg-black">
+    <div class="pt-10 px-10  pb-5 bg-black">
           <img class="w-32 h-32 rounded-full" :src="require('../assets/images/832.jpg')" alt="my image">
           <div class="flex mt-5">
           <i class="fas fa-map-marker text-gray mr-4"></i><h3 class=" text-gray-600 text-xs">Lagos, Nigeria</h3>
           </div>
-          
+
+          <div class="w-50 bg-black border border-t-amber-950 border-b-amber-950 border-r-black border-l-black  h-8  mt-5 mx-0 ">
+            <h2 class="font-mono mt-2 text-xs text-amber-800">  - MENU</h2>
+          </div>
     </div>
   </template>
 
