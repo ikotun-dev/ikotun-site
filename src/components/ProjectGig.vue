8b75bd93d750f292252f777bc220359d1fb8fa33
Ikotun Collins <danlogan2003@gmail.com>
2023-06-26 18:11:30 +0100

    last layer

--- src/components/ProjectGig.vue
+++ src/components/ProjectGig.vue
@@ -3,7 +3,7 @@
     <h3 class="font-extrabold text-xl text-gray-100 .wave-emoji ml-12 py-3"> Projects &#128193;</h3>
     </div>
     <div class="bg-black">
-        <h3 class="text-gray-400 text-sm ml-12">I build stuffs I want to use , as a software engineer, I spend my free time immersed in learning new technologies and honing my skills, constantly exploring innovative ways to build and create remarkable solutions. It's a fulfilling journey where passion meets knowledge, and I thrive in the realm of endless possibilities.</h3>
+        <h3 class="text-gray-400 text-sm ml-12">I build stuffs I want to use , as a software engineer, I spend my free time immersed in learning new technologies and honing my skills, constantly exploring innovative ways to build and create remarkable solutions. It's a fulfilling journey where passion meets knowledge, and I thrive in the realm of endless possibilities.<br/><h2 class="ml-12 mt-3 text-gray-300">&#128308; To mention a few &rightarrow;</h2></h3>
     </div>
     <div class=" px-5 py-10 bg-black pl-12 sm:flex-col lg:flex-row lg:flex-wrap lg:flex">
     <a  href="https://ikotun-dev.github.io/linkify-ui/">
@@ -44,6 +44,16 @@
         <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/nouss_img.png')"/>
         <h3 class=" sm:text-sm  sm:ml-2 font-pop font-bold lg:ml-8 mt-4 text-white">*Collab* - Backend for a TimeTable Generator <br/><br/>Technologies<br/>
         <h2 class="text-sm "> Backend  &rightarrow;  python <br><h2 class="text-gray-500"> Frontend Credit &rightarrow; Tejiri <i class="fas fa-female"></i></h2></h2></h3>
+    </div>
+    <div class=" h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4 sm:mt-6 lg:mt-3 lg:ml-6">
+        <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/sids_img.png')"/>
+        <h3 class=" sm:text-sm  sm:ml-2 font-pop font-bold lg:ml-8 mt-4 text-white">( Unreleased) CORAL - ecommerce for clothing and accessories <br/><br/>Technologies<br/>
+        <h2 class="text-sm "> Frontend  &rightarrow;  vueJs <br><h2> Backend &rightarrow; django </h2></h2></h3>
+    </div>
+    <div class=" h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4 sm:mt-6 lg:mt-3 lg:ml-6">
+        <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/car_img.png')"/>
+        <h3 class=" sm:text-sm  sm:ml-2 font-pop font-bold lg:ml-8 mt-4 text-white">Car Price Analysis (DS)<br/><br/>Technologies<br/>
+        <h2 class="text-sm ">  Language &rightarrow;  Python <br><h2> Extra &rightarrow; Jupyter </h2></h2></h3>
     </div>
  </div>
 </template>
