55e6f113a338ba566185bdf724464e2a79db7d08
Ikotun Collins <danlogan2003@gmail.com>
2023-06-26 14:20:55 +0100

    3rd proj addition

--- src/components/ProjectGig.vue
+++ src/components/ProjectGig.vue
@@ -1,16 +1,27 @@
 <template>
    <div class=" px-5 py-10 bg-black pl-10 sm:flex-col lg:flex-row lg:flex">
-    <div class=" sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4">
+    <a  href="https://ikotun-dev.github.io/linkify-ui/">
+    <div  class=" h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4">
         <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/linkify_img.png')"/>
         <h3 class=" sm:text-sm lg:text-lg sm:ml-2 font-pop font-bold lg:ml-8 text-white"> Linkify - application for shortening Long URLs. <br/><br/>Technologies<br/>
         <h2 class="text-sm">Frontend &rightarrow;  vueJs <br> Backend &rightarrow; fastapi (python) </h2></h3>
+    </div>
+    </a>
+
+    <div class=" h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4 sm:mt-6 lg:mt-0 lg:ml-6">
+        <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/gigflut_img.png')"/>
+        <h3 class=" sm:text-sm lg:text-lg sm:ml-2 font-pop font-bold lg:ml-8 mt-4 text-white"> ( Unreleased ) GigFlut - Job Board <br/><br/>Technologies<br/>
+        <h2 class="text-sm">Frontend &rightarrow;  vueJs <br> Backend &rightarrow; django (python) </h2></h3>
     </div>
 
-    <div class=" sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4">
-        <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/linkify_img.png')"/>
-        <h3 class=" sm:text-sm lg:text-lg sm:ml-2 font-pop font-bold lg:ml-8 text-white"> Linkify - application for shortening Long URLs. <br/><br/>Technologies<br/>
-        <h2 class="text-sm">Frontend &rightarrow;  vueJs <br> Backend &rightarrow; fastapi (python) </h2></h3>
+    <a href="https://ikotun-dev.github.io/infos/">
+    <div class=" h-96 sm:ml-2 border-b border-l border-r border-gray-800 rounded-lg w-100 sm:w-72 pb-4 sm:mt-6 lg:mt-0 lg:ml-6">
+        <img class="h-72 sm:h-48  rounded-t-9xl" :src="require('../assets/images/infos_img.png')"/>
+        <h3 class=" sm:text-sm lg:text-lg sm:ml-2 font-pop font-bold lg:ml-8 mt-4 text-white"> InFos - article page <br/><br/>Technologies<br/>
+        <h2 class="text-sm">Frontend &rightarrow;  HTML $ CSS </h2></h3>
     </div>
+    </a>
+
 
     </div> 
     <div></div>
